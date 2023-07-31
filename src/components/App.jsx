f1029db03afe8f258ad90bf76ea0fecf05025f12
FX-IT-Marina <121512261+FX-IT-Marina@users.noreply.github.com>
2023-07-31 19:16:56 +0200

    done hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { AppWrapper } from './App.styled';
+
+export class App extends Component {
+  state = {
+    imageName: '',
+  };
+
+  handleFormSubmit = imageName => {
+    this.setState({ imageName });
+  };
+
+  render() {
+    return (
+      <AppWrapper>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery imageName={this.state.imageName} />
+      </AppWrapper>
+    );
+  }
+}
